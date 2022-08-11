c4eedce8036c13c3d654ee810fea034d91b8c9db
eqNd <emregundogdu0@hotmail.com>
2022-08-11 12:35:21 +0300

    denedik

---
+++ library/commands/eval.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder } = require('discord.js');
+const { inspect } = require('util');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName('eval')
+  .setDescription('Evaluates code for Bot Developer.')
+  .toJSON(),
+  execute: async interaction => {
+    if (interaction.user.id != interaction.client.properties.developerId) return;
+
+    const row = new ActionRowBuilder().addComponents([new TextInputBuilder()
+    .setLabel('Code')
+    .setCustomId('code')
+    .setValue(interaction.client.database.get('last-evaluated-code') || '')
+    .setStyle(2)
+    .setPlaceholder('Code to evaluate.')
+    .setRequired(true)]);
+
+    return await interaction.showModal(new ModalBuilder().setCustomId('evulate-code').setTitle('Evulate Code').addComponents([row]));
+  },
+  events: {
+    submit: { 
+      'evulate-code': async interaction => {
+        await interaction.deferReply({ ephemeral: true });
+        let code = interaction.fields.getTextInputValue('code');
+        await interaction.client.database.set('last-evaluated-code', code);
+    
+        try {
+          const result = await eval(!code.includes('await') ? code : `(async () => { ${code} })()`);
+          return interaction.followUp({ content: '```js\n' + inspect(result, { depth: 0 }).slice(0, 1990) + '```' });
+        } catch(error) {
+          return interaction.followUp({ content: '```js\n' + inspect(error, { depth: 0 }).slice(0, 1990) + '```' });
+        };
+      }
+    }
+  }
+};
---
+++ library/modules/Application.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  token: 'MTAwNzIxOTE4MzExODk5MTM4Mw.G_K5I3.F0x8x8VhLYuBtVtkTPSmZ9XghJUeQF6Ig5_huw',
+  developerId: '377797295548268547'
+};
